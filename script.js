41c5eb1aa78154b1520dede5d3da724a851b5f2e
Railson de Oliveira Silva <railson.de.oliveira.silva.ros@gmail.com>
2025-08-20 20:05:54 -0300

    teste

--- script.js
+++ script.js
@@ -1,18 +1,13 @@
 const tarefas = ["Estudar HTML", "Estudar CSS", "Estudar JS"]
 
+
 function render() {
     const ul = document.querySelector("ul")
-    ul.innerHTML = ""
-    tarefas.forEach(function(tarefa){
-        const li = document.createElement("li")
-        li.innerText = tarefa
-        ul.appendChild(li)
-    })
+        ul.innerHTML = ""
+        tarefas.forEach(function(tarefa){
+            const li = document.createElement("li")
+            li.innerText = tarefa
+            ul.appendChild(li)          
+        })    
 }
-function add_new_task() {
-    const input = document.querySelector("input")    
-    tarefas.push(input.value)
-    input.value = ""
-    render()
-}   
 render()
